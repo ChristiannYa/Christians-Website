86d28f7728d160f60f8aaccc7879933d5cc082c4
ChristiannYa <christian.ylopez02@gmail.com>
2025-09-30 14:31:44 -0500

    info update

--- client/src/sections/Intro.jsx
+++ client/src/sections/Intro.jsx
@@ -19,19 +19,24 @@ const Intro = () => {
           <h1 className="text-acc-1 font-robot text-4xl mb-1">
             Hi I am christian
           </h1>
-          <div className="w-[400px] max-md:w-full flexcol gap-y-2">
-            <p className="paragraph-lg leading-none font-[600]">
-              Full-Stack Web Developer
-            </p>
-            <p className="paragraph-lg">
-              I build visually engaging websites, with a focus on simplicity and
-              elegance. My goal is to ensure that users can easily navigate
-              through a clean and well crafted site.
-            </p>
-            <p className="paragraph-lg">
-              Outside of building websites, you&apos;ll find me playing chess or
-              watching TV shows and movies.
-            </p>
+          <div className="w-[400px] max-md:w-full flexcol gap-y-1">
+            <div>
+              <p className="paragraph-lg leading-none font-medium">
+                Full-Stack Web Developer
+              </p>
+              <p className="paragraph opacity-80">Based in Houston, TX</p>
+            </div>
+            <div className="space-y-1">
+              <p className="paragraph-lg">
+                I build visually engaging websites, with a focus on simplicity
+                and elegance. My goal is to ensure that users can easily
+                navigate through a clean and well crafted site.
+              </p>
+              <p className="paragraph-lg">
+                Outside of building websites, you&apos;ll find me playing chess
+                or watching TV shows and movies.
+              </p>
+            </div>
             <div className="overflow-hidden rounded-full w-[250px] aspect-square max-md:w-[200px] max-md:my-6 max-md:mx-auto mt-0.5">
               <img
                 src={christian}
